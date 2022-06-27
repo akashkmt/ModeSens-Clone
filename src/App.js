bea390eafd9601571f4e5c1f944321b1c870eab9
Aausaf Alam <aausafalam585@gmail.com>
2022-06-27 03:56:46 -0700

    Home Page Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+ import Home from './Components/Example/Home/Home';
 function App() {
   return (
-    <div className="App">
-      MODESENS CLONE
-    </div>
+    <>
+      <div style={{marginLeft:"-1.3vh"}} className="h">
+      <Home></Home>
+      </div>
+    </>
   );
 }
 
