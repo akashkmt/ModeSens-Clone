d045fc2feb645191ce438ce1c7fcaedc9fc8db69
pk393256 <pk393256@gmail.com>
2022-06-29 22:29:31 +0530

    first_commit
    first

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
-import Checkout from './Components/Checkout/Checkout';
-import Navbar from './component/Navbar/Navbar';
-import {BrowserRouter} from "react-router-dom"
-import {Footer} from "./component/Footer/Footer"
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  theme,
+} from '@chakra-ui/react';
+import Men from "./page/men";
+import Women from './page/women';
+import Beauty from './page/beauty';
+import ItemDetails from './page/itemDetails';
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
-    <Checkout />
-    <Footer/>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl" >
+      <Routes>
+        <Route path="/mens" element={<Men />} />
+        <Route path='/womens' element={<Women />} />
+        <Route path='/beauty' element={<Beauty />} />
+        <Route path='/mens/:productId'  element={<ItemDetails />}/>
+        <Route path='/womens/:productId'  element={<ItemDetails />}/>
+        <Route path='/beauty/:productId'  element={<ItemDetails />}/>
+      </Routes>
+      </Box>
+    </ChakraProvider>
   );
 }
 
