121791ec755fe04d0cfb69a89046dca542a342d2
Aausaf Alam <aausafalam585@gmail.com>
2022-07-01 06:12:42 -0700

    updated Home page and Navbar bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,24 @@ import Beauty from './Components/Page/Beauty';
 import ItemDetails from './Components/Page/ItemDetails';
 import { Routes, Route } from "react-router-dom";
 import './App.css'
-import Navbar from './Components/Navbar/Navbar';
+//import Navbar from './Components/Navbar/Navbar';
 import Checkout from './Components/Checkout/Checkout';
 import {Footer} from './Components/Footer/Footer';
-
-
+import Navbar from './Components/Navbars/Navbar';
+import Home from './Components/Home/Home';
 function App() {
   return (
     
       <div className='App' style={{textAlign:'center'}} >
-        <Navbar />
+       <Navbar/>
       <Routes>
+        {/* please Install these packages:
+        //npm i react-material-ui-carousel
+        //npm i slick-carousel
+        //npm i react-icons
+        //npm install react-slick
+        //npm i react-items-carousel    */}
+        <Route path="/" element={<Home></Home>}/>
         <Route path="/Mens" element={<Men />} />
         <Route path='/Womens' element={<Women />} />
         <Route path='/Beauty' element={<Beauty />} />
