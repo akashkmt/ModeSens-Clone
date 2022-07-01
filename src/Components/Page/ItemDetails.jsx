0e4589480f7f4ab2872b52c1c176a6303cff9488
pk393256 <pk393256@gmail.com>
2022-07-01 19:35:09 +0530

    aa

--- src/Components/Page/ItemDetails.jsx
+++ src/Components/Page/ItemDetails.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const ItemDetails=()=>{
     const { productId } = useParams();
+    console.log(productId)
     var currentLocation = window.location.pathname;
     console.log(currentLocation)
+    // fetch('http://localhost:8080')
 
     return(
-        <h1>{currentLocation}</h1>
+        <h1>{currentLocation}{'--'}{productId}</h1>
     )
 }
 export default ItemDetails;
